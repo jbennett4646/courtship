379fba48009c7af049e630b0720b10b0087b63e7
Husnain-Abid <jbennett4646@gmail.com>
2026-01-26 17:36:23 +0500

    first

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
 
+
 const Logo: React.FC = () => {
   return (
     <div className="relative h-12 w-40">
       <Image
-        src="https://assets.co.dev/ba0d4dc8-2012-4d82-94f5-65cb468b94f3/capture2-removebg-preview-38d1ee3.png"
+        src="/logo.png"
         alt="Courtingly Logo"
         fill
         className="object-contain"
